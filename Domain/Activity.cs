e757eec63498423c89d1aab663245bf6ae79d92a
nghgphuo <phuocnguyen36398@gmail.com>
2025-09-09 11:42:52 +0700

    creating the entity framework dbcontext class

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,26 +1,27 @@
-namespace Domain;
-
-public class Activity
+namespace Domain
 {
-    public string Id {get; set;} = Guid.NewGuid().ToString();
+    public class Activity
+    {
+        public string Id {get; set;} = Guid.NewGuid().ToString();
 
-    public required string Title {get; set;}
+        public required string Title {get; set;}
 
-    public DateTime Date {get;set;}
+        public DateTime Date {get;set;}
 
-    public required string Description {get; set;}
+        public required string Description {get; set;}
 
-    public required string Category {get; set;}
-    
-    public bool isCancelled {get;set;}
+        public required string Category {get; set;}
+        
+        public bool isCancelled {get;set;}
 
-    // location props
+        // location props
 
-    public string Venue {get; set;}
+        public string Venue {get; set;}
 
-    public string City {get; set;}
+        public string City {get; set;}
 
-    public double Latitude {get; set;}
-    
-    public double Longitude {get; set;}
+        public double Latitude {get; set;}
+        
+        public double Longitude {get; set;}
+    }
 }
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Activity> Activities {get; set;}
+    }
+}

5efb8a9fce7e7e6e05674e53bc409166e2dd1936
nghgphuo <phuocnguyen36398@gmail.com>
2025-09-29 16:51:10 +0700

    add https and axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch("https:localhost:5001/api/activities")
-      .then((response) => response.json())
-      .then((data) => setActivities(data));
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
 
     return () => {};
   }, []);

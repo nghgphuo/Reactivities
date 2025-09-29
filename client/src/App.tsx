73b224065d25b892c63ad242632aa47619853d54
nghgphuo <phuocnguyen36398@gmail.com>
2025-09-29 16:06:10 +0700

    create Activity type and add Material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch("https:localhost:5001/api/activities")
       .then((response) => response.json())
       .then((data) => setActivities(data));
+
+    return () => {};
   }, []);
 
   return (
-    <div>
-      <h3 className="app" style={{ color: "red" }}>
-        Reactivities
-      </h3>
-      <ul>
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   );
 }
 
